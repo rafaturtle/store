dfa013eb55d5d1348cfe04166fc961eef1eda67d
Rafael Amaral <rafael.b.amaral@gmail.com>
2013-05-11 11:26:54 +1000

    folder change

--- admin/config.php
+++ admin/config.php
@@ -1,24 +1,24 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://ec2-54-252-18-25.ap-southeast-2.compute.amazonaws.com/opencart/admin/');
-define('HTTP_CATALOG', 'http://ec2-54-252-18-25.ap-southeast-2.compute.amazonaws.com/opencart/');
+define('HTTP_SERVER', 'http://ec2-54-252-18-25.ap-southeast-2.compute.amazonaws.com/store/admin/');
+define('HTTP_CATALOG', 'http://ec2-54-252-18-25.ap-southeast-2.compute.amazonaws.com/store/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://ec2-54-252-18-25.ap-southeast-2.compute.amazonaws.com/opencart/admin/');
-define('HTTPS_CATALOG', 'http://ec2-54-252-18-25.ap-southeast-2.compute.amazonaws.com/opencart/');
+define('HTTPS_SERVER', 'http://ec2-54-252-18-25.ap-southeast-2.compute.amazonaws.com/store/admin/');
+define('HTTPS_CATALOG', 'http://ec2-54-252-18-25.ap-southeast-2.compute.amazonaws.com/store/');
 
 // DIR
-define('DIR_APPLICATION', 'C:\xampp\htdocs\opencart/admin/');
-define('DIR_SYSTEM', 'C:\xampp\htdocs\opencart/system/');
-define('DIR_DATABASE', 'C:\xampp\htdocs\opencart/system/database/');
-define('DIR_LANGUAGE', 'C:\xampp\htdocs\opencart/admin/language/');
-define('DIR_TEMPLATE', 'C:\xampp\htdocs\opencart/admin/view/template/');
-define('DIR_CONFIG', 'C:\xampp\htdocs\opencart/system/config/');
-define('DIR_IMAGE', 'C:\xampp\htdocs\opencart/image/');
-define('DIR_CACHE', 'C:\xampp\htdocs\opencart/system/cache/');
-define('DIR_DOWNLOAD', 'C:\xampp\htdocs\opencart/download/');
-define('DIR_LOGS', 'C:\xampp\htdocs\opencart/system/logs/');
-define('DIR_CATALOG', 'C:\xampp\htdocs\opencart/catalog/');
+define('DIR_APPLICATION', 'C:\xampp\htdocs\store/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\store/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\store/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\store/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\store/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\store/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\store/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\store/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\store/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\store/system/logs/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\store/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysql');
--- config.php
+++ config.php
@@ -1,21 +1,21 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://ec2-54-252-18-25.ap-southeast-2.compute.amazonaws.com/opencart/');
+define('HTTP_SERVER', 'http://ec2-54-252-18-25.ap-southeast-2.compute.amazonaws.com/store/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://ec2-54-252-18-25.ap-southeast-2.compute.amazonaws.com/opencart/');
+define('HTTPS_SERVER', 'http://ec2-54-252-18-25.ap-southeast-2.compute.amazonaws.com/store/');
 
 // DIR
-define('DIR_APPLICATION', 'C:\xampp\htdocs\opencart/catalog/');
-define('DIR_SYSTEM', 'C:\xampp\htdocs\opencart/system/');
-define('DIR_DATABASE', 'C:\xampp\htdocs\opencart/system/database/');
-define('DIR_LANGUAGE', 'C:\xampp\htdocs\opencart/catalog/language/');
-define('DIR_TEMPLATE', 'C:\xampp\htdocs\opencart/catalog/view/theme/');
-define('DIR_CONFIG', 'C:\xampp\htdocs\opencart/system/config/');
-define('DIR_IMAGE', 'C:\xampp\htdocs\opencart/image/');
-define('DIR_CACHE', 'C:\xampp\htdocs\opencart/system/cache/');
-define('DIR_DOWNLOAD', 'C:\xampp\htdocs\opencart/download/');
-define('DIR_LOGS', 'C:\xampp\htdocs\opencart/system/logs/');
+define('DIR_APPLICATION', 'C:\xampp\htdocs\store/catalog/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\store/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\store/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\store/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\store/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\store/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\store/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\store/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\store/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\store/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysql');
